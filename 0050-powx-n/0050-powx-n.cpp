4515d0491da4e47bd5dd1cc095a29cf27e4cec54
s-gupta28 <harekrishna28q@gmail.com>
2025-07-04 12:49:49 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.07%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,33 +1,29 @@
 class Solution {
-private:
-    double helper(double x,long long n ){
-         if(x==0){
-            return 0;
-         }
-         if(n==0){
-            return 1;
-         }
-         double res= helper(x,n/2);
-         //if odd power
-         if(n%2==1){
-            return x*res*res;
-         }
-         else{
-            return res*res;
-         }
-
-    }
 public:
     double myPow(double x, int n) {
-        long  long N=n;
-        double res;
-        res=helper(x,abs(N));
-        if(n>=0){
-            return res;
+        double ans= 1.0;
+        // n is stored as it's value will be chnaged
+        long long nn=n;
+        if (nn<0){
+            nn=-1*nn;
         }
-        else{
-            return 1/res;
+
+        while(nn!=0){
+            // power is odd
+            if(nn%2==1){
+                ans=ans*x;
+                nn=nn-1;
+
+            }
+            else{
+                x=x*x;
+                nn=nn/2;
+            }
         }
-        
+
+        if(n<0){
+            ans= (double)(1.0)/(double)(ans);
+        }
+        return ans;
     }
 };
