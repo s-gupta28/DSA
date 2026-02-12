a1ab7cf5b32711e3d6c0425a094bb872e533cd21
s-gupta28 <harekrishna28q@gmail.com>
2026-02-12 10:28:13 +0530

    Time: 0 ms (100%), Space: 11.3 MB (16.63%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,36 +1,24 @@
 class Solution {
-    private:
-    void printVec(vector<int> vector){
-        for(int i=0; i<vector.size(); i++){
-            cout<<vector[i];
-        }
-    }
-
 public:
     bool check(vector<int>& nums) {
-        if(nums.size()==1){
+        int n=nums.size();
+        if(n==1){
             return true;
         }
-        vector<int> temp=nums;
-        //contacatinating same array
-        for(int i=0;i<nums.size();i++){
-            temp.push_back(nums[i]);
-        }
-        
-        int left=0;
-        int right=0;
-        int size=1;
-        
-        int i=1;//ALWAYS HERE as i+1 cause error
-        while(i<temp.size()){
-            if(size==nums.size()){
-                return true;
-            }
-            if(temp[i-1]<temp[i]){
-                size++;
+        int count=1;
+        int i=1;
+        //ERROR always use i if using while
+        // here we are trying to iterate the array twice
+        while(i<(2*n)){
+            if(nums[(i-1)%n]<=nums[i%n]){
+                count++;
             }
             else{
-                size=1;
+                count=1;
+            }
+            // this correct pos of if
+            if(count==n){
+                return true;
             }
             i++;
         }
